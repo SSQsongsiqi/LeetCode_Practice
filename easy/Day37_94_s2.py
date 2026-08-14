81c3681dbad1f540ea6d3869f196ce8f886d3fde
SSQsongsiqi <119599545+SSQsongsiqi@users.noreply.github.com>
2026-08-14 22:42:54 +0800

    Add inorderTraversal method for binary tree
    Implement inorder traversal of a binary tree using Morris traversal.

---
+++ easy/Day37_94_s2.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        current = root
+
+        while current:
+            if current.left is None:
+                result.append(current.val)
+                current = current.right
+
+            else:
+                predecessor = current.left
+
+                while predecessor.right and predecessor.right != current:
+                    predecessor = predecessor.right
+
+                if predecessor.right is None:
+                    predecessor.right = current
+                    current = current.left
+
+                else:
+                    predecessor.right = None
+                    result.append(current.val)
+                    current = current.right
+
+        return result
